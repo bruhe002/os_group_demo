20c5a921f65ebfda5dc214d9971168fe2c9b84fa
Eric Bruhns <bruhe002@gmail.com>
2023-05-01 22:55:27 -0700

    Implemented needed variables

--- mfc_simulator.cpp
+++ mfc_simulator.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <iomanip>
 #include <queue>
+#include <array>
 
 using namespace std;
 
 int main() {
-
-
     // Simulating MFC preemptive scheduling for threads
 
     // Create a struct to simulate threads
@@ -14,7 +13,8 @@ int main() {
         int tid;        // Thread ID
         int priority;   // Priority number
         int burst_time; // Execution time
-        int arrival_time; // Time of Arrival 
+        int arrival_time; // Time of Arrival
+        int needed_resource;    // Used to simulate resources being used 
     };
 
     // Create the Priority Queues
@@ -26,6 +26,17 @@ int main() {
     queue<sim_thread> THREAD_PRIORITY_HIGHEST;
     queue<sim_thread> THREAD_PRIORITY_TIME_CRITICAL;
 
+    // Have a blocked queue
+    queue<sim_thread> BLOCKED_THREADS;
+
+    // For simplicity, we will use one time slice for all queues
+    int time_slice = 2;         // 2 milliseconds
+
+    // We will use an array of boolean values to simulate resources being taken
+    bool resources[100];
+    for(int i = 0; i < 100; i++) {
+        resources[i] = false;
+    }
 
     cout << endl;
     system("Pause");
